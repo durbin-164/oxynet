ebb82214dcf4239e6d50bd0903b246252ffb8424
Md. Masud Rana <masud.rana@infolytx.com>
2020-06-09 23:45:50 +0600

    add mul function

--- oxynet/main.py
+++ oxynet/main.py
@@ -6,7 +6,11 @@ def add(a, b):
 def sub(a,b):
     return a-b
 
+def mul(a,b):
+    return a*b
+
 
 if __name__ == "__main__":
     print(add(10, 20))
     print(sub(10,20))
+    print(mul(10,20))
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,10 @@
-from oxynet.main import add, sub
+from oxynet.main import add, sub, mul
 
 def test_add():
     assert add(100,200) == 300
 
 def test_sub():
     assert sub(200, 100) == 100
+
+def test_mul():
+    assert mul(10,10) == 100
