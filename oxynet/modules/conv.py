f8558ffc7b6d52fcee982ba2560d114bf9449a43
Md. Masud Rana <masud.rana@infolytx.com>
2020-07-08 22:57:40 +0600

    impliment convolution 2D

--- oxynet/modules/conv.py
+++ oxynet/modules/conv.py
@@ -2,39 +2,51 @@ from typing import Tuple
 from oxynet import Tensor 
 from oxynet.modules import Module , Parameter
 import numpy as np
+from .higher_dimention_ops import img2col, get_conv_output_shape
 
 class Conv2d(Module):
     def __init__(self, 
                  in_channels: int,
                  out_channels: int,
-                 kernel_size: int,
-                 stride: int =1)->None:
+                 kernel_size: Tuple,
+                 stride: int =1,
+                 pad: int = 0,
+                 )->None:
         super().__init__()
 
         self.in_channels = in_channels
         self.out_channels = out_channels
         self.kernel_size = kernel_size
         self.stride = stride
-        self.kernels = []
+        self.pad = pad
+        
+        self.weight = None
+        self.bias = None
         
         self.initialize_weights()
 
     def initialize_weights(self)-> None:
-        for _ in range(self.out_channels):
-            self.kernels.append( Parameter(self.kernel_size, self.kernel_size,self.in_channels))
+        k_h, k_W = self.kernel_size
+        self.weight = Parameter(self.in_channels*k_h*k_W, self.out_channels)
+        self.bias = Parameter(self.out_channels)
 
     def forward(self, input : Tensor)-> Tensor:
-        b, h,w, _ = input.shape
-        temp = self.kernel_size//2
-        output = Tensor(np.zeros((b, h-temp*2,w-temp*2,self.out_channels)))
-        for b, image in enumerate(input):
-            for o , kernel in enumerate(self.kernels):
-                for i in range(h-self.kernel_size):
-                    for j in range( w-self.kernel_size):
-                        output[b,i, j,o] = (image[i:self.kernel_size, j:self.kernel_size] * kernel).sum()
+        
+        N,_,H,W = input.shape
+        out_h,out_w = get_conv_output_shape(H,W,self.kernel_size,self.stride, self.pad)
+        
+        # col_input shape = (N*out_h*out_w, C*k_h*k_w)
+        col_input = img2col(input, self.kernel_size, self.stride, self.pad)
+        
+        '''
+        col_input = (N*out_h*out_w, C *k_h * k_w) @ (C*k_h * k_w, out_channel) = self.weight
+        shape [N*out_h*out_h, filter_nums]
+        '''
+        col_output = col_input @ self.weight + self.bias
 
-
-        return Tensor(output)
+        output = col_output.reshape(N, out_h, out_w, -1).transpose(0,3,1,2)
+       
+        return output
 
 
 
--- tests/modules/conv_test.py
+++ tests/modules/conv_test.py
@@ -1,28 +1,36 @@
-# from unittest import TestCase 
-# from oxynet.modules import Conv2d , Flatten
-# from oxynet import Tensor 
-# import numpy as np 
+from unittest import TestCase 
+from oxynet.modules import Conv2d , Flatten
+from oxynet import Tensor 
+import numpy as np 
 
-# class TestConv2d(TestCase):
-#     def test_simple_cov2d(self):
-#         input = Tensor(np.random.randn(2,8,8,3), requires_grad= True)
+class TestConv2d(TestCase):
+    def test_simple_cov2d(self):
+        input = Tensor(np.random.randn(2,3,8,8), requires_grad= True)
 
-#         # print(input.shape)
+        # print(input.shape)
         
-#         conv = Conv2d(3, 5,5)
+        conv = Conv2d(3, 5,(5,5))
 
-#         output = conv(input)
+        output = conv(input)
 
-#         assert output.shape == (2,4,4,5)
+        assert output.shape == (2,5,4,4)
 
-#         ######
-#         input = Tensor(np.random.randn(3,8,9,3), requires_grad= True)
+        output.backward(output)
+
+        assert input.grad.shape == (2,3,8,8)
+
+        ######
+        input = Tensor(np.random.randn(3,3,8,9), requires_grad= True)
         
-#         conv = Conv2d(3, 10,3)
+        conv = Conv2d(3, 10,(3,3), 2)
 
-#         output = conv(input)
+        output = conv(input)
 
-#         assert output.shape == (3,6,7,10)
+        assert output.shape == (3,10,3,4)
+
+        output.backward(output)
+
+        assert input.grad.shape == (3,3,8,9)
 
 
 # class TestFlatten(TestCase):
