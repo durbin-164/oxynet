9d433128ac999f8aae4cc66c553a1124dbd052d3
Md. Masud Rana <masud.rana@infolytx.com>
2020-07-18 01:06:43 +0600

    fixed activation test

--- tests/modules/activation_test.py
+++ tests/modules/activation_test.py
@@ -14,6 +14,8 @@ class TestSoftmax(unittest.TestCase):
         expected = cp.array([0.090031, 0.244728, 0.665241])
         cp.testing.assert_array_almost_equal(output.data, expected)
 
+        assert output.data.sum().tolist() == 0.9999999999999998
+
         #TODO: Test backward
         # grad_data = Tensor([1,1,1])
         # output.backward(grad_data)
@@ -21,6 +23,17 @@ class TestSoftmax(unittest.TestCase):
         
         # expected = input.data * (1-input.data)
         # np.testing.assert_array_almost_equal(input.grad.data, expected)
+
+    def test_2d_softmax(self):
+        # softmax = Softmax()
+
+        input = Tensor([[1,2,3],[1,2,3]], requires_grad=True)
+        output = F.softmax(input)
+
+        expected = cp.array([[0.090031, 0.244728, 0.665241],[0.090031, 0.244728, 0.665241]])
+        cp.testing.assert_array_almost_equal(output.data, expected)
+
+        assert output.data.sum(axis=-1).tolist() == [0.9999999999999998,0.9999999999999998]
 
 
 class TestTanh(unittest.TestCase):
